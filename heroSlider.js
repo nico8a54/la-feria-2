987139090be13aee38217397b97144ae4fa139a5
nico8a54 <68613951+nico8a54@users.noreply.github.com>
2022-02-28 16:35:45 -0500

    v0.1.1
    Agregue linea y control desde js

--- heroSlider.js
+++ heroSlider.js
@@ -1,3 +1,9 @@
+const line1 = document.getElementById("line1");
+line1.style.backgroundColor = "red";
+
+const myElement = document.getElementById("demo");
+myElement.style.color = "red";
+
 var counter = 1;
 setInterval(function(){
     document.getElementById('radio' + counter).checked = true;
