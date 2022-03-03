8004d2489f5c3351472bbe41df160c9417c888cb
nico8a54 <68613951+nico8a54@users.noreply.github.com>
2022-03-02 21:47:08 -0500

    v0.1.2
    Hero slide simplificado

--- heroSlider.js
+++ heroSlider.js
@@ -1,12 +1,10 @@
-const line1 = document.getElementById("line1");
-line1.style.backgroundColor = "red";
-
-const myElement = document.getElementById("demo");
-myElement.style.color = "red";
-
 var counter = 1;
+const line = document.getElementById('manual-btn-' + counter);
+
+
 setInterval(function(){
     document.getElementById('radio' + counter).checked = true;
+    line.style.backgroundColor = '#3A37E0';
     counter ++;
     if(counter>3){
         counter = 1;
