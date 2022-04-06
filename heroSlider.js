f3f6a09f744ba481af4de1157bad797f5d94fb91
nico8a54 <68613951+nico8a54@users.noreply.github.com>
2022-04-06 17:17:27 -0500

    v5

--- heroSlider.js
+++ heroSlider.js
@@ -5,11 +5,13 @@ var title1 = document.getElementById('title1')
 var title2 = document.getElementById('title2')
 var title3 = document.getElementById('title3')
 
+video1.play();
+video1.style.opacity=1;
+
 video1.onended = function () {
     video2.play();
     video1.style.opacity=0;
     video2.style.opacity=1;
-    video2.style.animationName="slide2";
     video3.style.opacity=0;
     title1.style.display="none";
     title2.style.display="flex";
