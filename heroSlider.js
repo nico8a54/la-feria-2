03253a2f48419857f21d02179252e4d0ed1006fb
nico8a54 <68613951+nico8a54@users.noreply.github.com>
2022-04-08 16:45:37 -0500

    v7

--- heroSlider.js
+++ heroSlider.js
@@ -4,6 +4,7 @@ var video3 = document.getElementById('video3')
 var title1 = document.getElementById('title1')
 var title2 = document.getElementById('title2')
 var title3 = document.getElementById('title3')
+var slideCounter = document.getElementById("slideCounter")
 
 video1.play();
 video1.style.opacity=1;
@@ -13,10 +14,12 @@ video1.onended = function () {
     video1.style.opacity=0;
     video2.style.opacity=1;
     video3.style.opacity=0;
+    slideCounter.innerHTML = "02/03"
     title1.style.display="none";
     title2.style.display="flex";
     title3.style.display="none";
 }
+
 video2.onended = function () {
     video3.play();
     video1.style.opacity=0;
@@ -26,6 +29,7 @@ video2.onended = function () {
     title2.style.display="none";
     title3.style.display="flex";
 }
+
 video3.onended = function () {
     video1.play();
     video1.style.opacity=1;
