5c34f4391472fae4527f8c02a6b2af8383a3f33c
nico8a54 <68613951+nico8a54@users.noreply.github.com>
2022-04-19 19:28:05 -0500

    v20

---
+++ js/buttonMenu.js
@@ -0,0 +1,16 @@
+class buttonMenu extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+        <section class="section_buttonMenu">
+        <div class="section_buttonMenu-button" onclick="this.classList.toggle('active')"></div>
+        <div class="section_buttonMenu-container" id='buttonMenu_container'>
+            <div class="section_buttonMenu-container-circle1" id='circle1'></div>
+            <div class="section_buttonMenu-container-circle2"></div>
+            <div class="section_buttonMenu-container-circle3"></div>
+        </div>        
+        </section>
+        `
+    }
+}
+
+customElements.define('my-button-menu', buttonMenu)
--- js/index.js
+++ js/index.js
@@ -1,23 +1,28 @@
 
-const heroSection = document.getElementById("heroSection")
 function slideMenu() {   
     var element = document.getElementById("menu"); 
     element.classList.toggle("active");
 }
+function openButtonMenu() {   
+    var element = document.getElementById("buttonMenu_container"); 
+    var circle = document.getElementById('circle1')
+    element.classList.toggle("active");
+    circle.classList.toggle("active");
+}
 
+// Scroll Magic.
+    var controller = new ScrollMagic.Controller();
 
-var controller = new ScrollMagic.Controller();
+    var scene1 = new ScrollMagic.Scene({
+        triggerElement: '.section_somos',
+        duration: "100%",
+    })
 
-var scene1 = new ScrollMagic.Scene({
-    triggerElement: '.section_somos',
-    duration: "100%",
-})
+    .setClassToggle('.section_hero','active')
+    .addTo (controller)
 
-.setClassToggle('.section_hero','active')
-.addTo (controller)
-
-// get the current scroll offset for the start and end of the scene.
-var start = scene1.scrollOffset(0);
-var end = scene1.scrollOffset(1) + scene1.duration();
-console.log("the scene starts at", start, "and ends at", end);
+    // get the current scroll offset for the start and end of the scene.
+    var start = scene1.scrollOffset(0);
+    var end = scene1.scrollOffset(1) + scene1.duration();
+    console.log("the scene starts at", start, "and ends at", end);
 
